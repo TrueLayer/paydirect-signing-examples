a0573869f8ac3f0576e4be5f5f3ad5bf87065bcd
Alex Butler <alex.butler@truelayer.com>
2021-08-05 13:06:33 +0100

    Add python webhook verification example

---
+++ python/verify.py
@@ -0,0 +1,48 @@
+import argparse
+import jwt
+import sys
+import json
+import urllib
+from jwt import PyJWKSet
+from base64 import urlsafe_b64encode
+
+
+def build_jws(args):
+    """Constructs a full jwt using the body & detached jws signature args."""
+    body_base64 = str(urlsafe_b64encode(
+        args.webhook_body.encode("utf-8")), "utf-8").rstrip("=")
+    return args.tl_signature.replace("..", f".{body_base64}.")
+
+
+def fetch_public_key(jws):
+    """Downloads the jwks indicated in the jws header"""
+    jws_header = jwt.get_unverified_header(jws)
+
+    # jku/jwks_url should be expected truelayer url(s)
+    assert jws_header["jku"] == "https://webhooks.truelayer.com/.well-known/jwks" \
+        or jws_header["jku"] == "https://webhooks.truelayer-sandbox.com/.well-known/jwks"
+
+    # note: header hack needed to bypass cloudflare 403
+    req = urllib.request.Request(jws_header["jku"], headers={
+                                 'User-Agent': "jwks-client"})
+    with urllib.request.urlopen(req) as response:
+        res = json.load(response)
+        set = PyJWKSet.from_dict(res)
+        return next(k for k in set.keys if k.key_id == jws_header["kid"])
+
+
+parser = argparse.ArgumentParser(
+    description='CLI to verify webhook payloads + signatures.')
+parser.add_argument('--webhook-body', help='the unmodified webhook POST body')
+parser.add_argument(
+    '--tl-signature', help='the `X-TL-Signature` webhook POST header')
+
+jws = build_jws(parser.parse_args())
+public_key = fetch_public_key(jws)
+
+try:
+    jwt.decode(jws, public_key.key, algorithms=["RS512"])
+    print("Webhook verified ✓")
+except Exception as e:
+    print(f"Webhook verification failed: {e}")
+    sys.exit(1)
